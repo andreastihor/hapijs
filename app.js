004f77077b5f5259cb25f90790d46fa50c78a5bc
andreas <andreastihor@gmail.com>
2019-01-30 15:25:02 +0700

    authorize and cookie done!, restricted also done.

--- app.js
+++ app.js
@@ -6,20 +6,18 @@ const authBasic = require('hapi-auth-basic');
 const authCookie = require('hapi-auth-cookie');
 const Bcrypt = require('bcryptjs')
 
-const {Article} = require('./Model/Articles.js')
+const {Article} = require('./models/Articles.js')
+const {User} = require('./models/User.js')
 
 const server = Hapi.server({
   port : 3000,
   host: 'localhost'
 });
 
-const users = {
-  andreas : {
-    username: 'andreas@gmail.com',
-password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
-name: 'andreas tihor',
-id: '1'
-}
+
+
+const validateJWT = async function (decoded, request) {
+  return { isValid: true, ...decoded };
 };
 
 const runServer = async () => {
@@ -28,13 +26,44 @@ const runServer = async () => {
   await server.register(authBasic);
   server.auth.strategy('simple','basic' , {validate});
   server.auth.strategy('restricted', 'cookie',{
-    password : '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',
+    password : '$2a$10$JwbwopKOwGepKZ/bRbFjB.1Av0HMxxbmGYDeofT55db1WdPEmIf82',
     cookie : 'session',
     isSecure : false,
     
-// biar ga redirect ke route yg pake try mode di strategy
-// redirectOnTry : false (error) 
-});
+	// biar ga redirect ke route yg pake try mode di strategy
+	// redirectOnTry : false (error) 
+	});
+
+	await server.register(require('hapi-auth-jwt2'));
+  server.auth.strategy('jwt', 'jwt', {
+  	key: 'secret',          // Never Share your secret key
+    validate: validateJWT,            // validate function defined above
+    verifyOptions: { algorithms: [ 'HS256' ] } // pick a strong algorithm
+  });
+
+  
+  server.route({
+    method : 'GET',
+    path : '/restricted' , 
+    options : {
+      handler : (request,h) => {
+        return "restricted page!"
+      },
+      auth :'restricted'
+    }
+  })
+
+  server.route({
+  	method: 'GET',
+  	path: '/decoded',
+  	handler: (request, h) => {
+  		console.log(request.auth.credentials)
+  		return 'test';
+  	},
+  	options: {
+  		auth: 'jwt'
+  	}
+  })
 
   try {
     server.route({
@@ -42,12 +71,16 @@ const runServer = async () => {
       path: '/login',
 
       options : {
-        handler : (req,res) => {
-          const user = users[req.payload.username];
-          const token = jwt.sign(user , 'secret');
-          return token
-          req.cookieAuth.set({token});
-          return res.redirect('/');
+        handler : (request, h) => {
+        	const { username } = request.payload;
+        	return User.findOne({ where: { username } })
+        		.then(user => {
+        			const { username, id, email, fullname } = user;
+        			const token = jwt.sign({ username, email, fullname, id }, 'secret');
+        			console.log(token)
+        			request.cookieAuth.set({token});
+        		})
+        	
         },
 
         validate: {
@@ -55,45 +88,11 @@ const runServer = async () => {
             username : Joi.string().required(), //email({minDomainsAtoms: 2})
             password : Joi.string().alphanum().required()
           }
-        },
-
-        auth : 'simple'
+        }
       }
 
     });
 
-    server.route({
-      method: 'POST',
-      path: '/register',
-
-      options :  {
-
-        handler : (req,res) => {
-
-          const user = {
-            fullname : res.payload.fullname,
-            email : res.payload.email,
-            password : Bcrypt(res.payload.password),
-            dob : res.payload.dob
-          }
-
-
-
-        },
-
-        validate : {
-          payload : {
-            fullname : Joi.string().min(5).required(),
-            password : Joi.string().alphanum().required().min(8),
-            username : Joi.string().min(5).required(),
-            email : Joi.string().min(5).required().email({minDomainsAtoms: 2}),
-            dob : Joi.number()
-          }
-        },
-
-        auth : 'restricted'
-      }
-    })
   } catch(err) {
     console.log(err);
     process.exit(1);
@@ -105,19 +104,25 @@ const runServer = async () => {
 
 const validate = async (request, username, password) => {
 
+  User.findOne({
+    where : {username}
+  })
+  .then((user) => {
+    const isValid =  Bcrypt.compareSync(password, user.password);
+    const credentials = { id: user.id, name: user.name };
 
-  const user = users[username];
-  if (!user) {
-    return { credentials: null, isValid: false };
-  }
+    return { isValid, credentials };
+  });
+  // const user = users[username];
+  // if (!user) {
+  //   return { credentials: null, isValid: false };
+  // }
 
-  const isValid = await Bcrypt.compare(password, user.password);
-  const credentials = { id: user.id, name: user.name };
+  // const isValid = await Bcrypt.compareSync(password, user.password);
+  // const credentials = { id: user.id, name: user.name };
 
-  return { isValid, credentials };
+  // return { isValid, credentials };
 };
-
-
 
 
 server.route( {
@@ -127,6 +132,7 @@ server.route( {
     handler : (req,res) => {
 
       const mod = Article.findAll();
+    
       return mod;
     },
 
@@ -135,10 +141,51 @@ server.route( {
   }
 });
 
+server.route({
+      method: 'POST',
+      path: '/register',
 
+      options :  {
 
+        handler : (req,res) => {
 
+        	const salt = Bcrypt.genSaltSync(10);
+					const hash = Bcrypt.hashSync(req.payload.password, salt);
+        	
+          const user = {
+            fullname : req.payload.fullname,
+            email : req.payload.email,
+            password : hash,
+            dob : req.payload.dob,
+            username : req.payload.username
 
+            
+          }
+
+          User.create({
+            	fullname : user.fullname,
+            	email : user.email,
+            	password : user.password,
+            	dob : user.dob,
+            	username : user.username
+           });
+          
+          return res.redirect('/')
+        },
+
+        validate : {
+          payload : {
+            fullname : Joi.string().min(5).required(),
+            password : Joi.string().alphanum().required().min(5),
+            username : Joi.string().min(5).required(),
+            email : Joi.string().min(5).required().email({minDomainsAtoms: 2}),
+            dob : Joi.number()
+          }
+        },
+
+       
+      }
+    })
 
 
 runServer();
